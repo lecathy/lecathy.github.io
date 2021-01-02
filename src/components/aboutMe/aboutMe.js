432c3052060359b20902b6c9f4e9dd24e7d76f04
lecathy <cathyle7.7@hotmail.com>
2021-01-01 23:25:41 -0500

    Replace placeholder text with an actual about me

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -4,7 +4,25 @@ export default function AboutMe() {
     return(
         <div className="about-me-container" id="aboutMe">
             <h1>About Me</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>
+                Hiiiiiiiii there! Thank you so much for checking this out! I'm Cathy and I'm a third year biomedical engineering student at the University of Waterloo.
+                I am passionate about developing and designing innovative software products for social good and am super eager for the future! I have a lot of experience with 
+                web development and design and would love to apply these skills to any exciting project!
+            </p>
+            <p>
+                In my spare time I enjoy doing a whole lot of artistic hobbies. In fact, all graphics found on my personal website were created by me from scratch
+                using&nbsp;
+                <u><a href="http://www.vectr.com" target="_blank" rel="noreferrer" style={{color: 'blue'}}>Vectr</a></u>!
+                I have also been playing piano since I was 5 and casually picked up guitar, uke and saxophone over the years.
+            </p>
+            <p>
+                At the end of 2020, I finished up an amazing co-op term with Cyclica where I was given loads of mentorship and opportunities!
+                I'll be going back to school for a term and am&nbsp;<b style={{ fontWeight: 600 }}>currently seeking co-op opportunities for Summer 2021!</b>
+            </p>
+            <p></p>
+            <i style={{textAlign: 'center'}}>
+                Feel free to reach out to me for a chat!
+            </i>
         </div>
     );
 }
